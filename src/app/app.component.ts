b5be3defe3c8b0907d9a0350440e293fa25e4eb2
Vab31 <97352743+Vab31@users.noreply.github.com>
2022-10-20 23:14:24 +0530

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,57 +7,66 @@ import { WeatherService } from './services/weather.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   title = 'weatherapp';
-  constructor(private weatherService:WeatherService){
-    
+  constructor(private weatherService: WeatherService) {
+
   }
   // weatherData?:WeatherData 
-  searchval : string='Mumbai';
+  searchval: string = 'Mumbai';
   stringJson: any;
   stringObject: any;
-  tempi:any;
-  flag:any;
-  humidty_val:any
-  wind_val:any
-  sunrise:any
-  heatindex:any
-  moonphase:any
+  tempi: any;
+  flag: any;
+  humidty_val: any
+  wind_val: any
+  sunrise: any
+  heatindex: any
+  moonphase: any
+  alerts: any;
 
 
 
-  
-  ngOnInit():void{
-    console.log(this.searchval)
-    this.flag=this.searchval;
-       this.weatherService.getWeatherData(this.searchval)
-       .subscribe({
-        next:(respone)=>
-        {
-          this.stringJson = JSON.stringify(respone);
-    console.log("String json object :", this.stringJson);
-    console.log("Type :", typeof this.stringJson);
+
+  ngOnInit(): void {
+                      console.log(this.searchval)
+                      this.flag = this.searchval;
+                      this.weatherService.getWeatherData(this.searchval)
+                        .subscribe({
+                          next: (respone) => {
+                            this.stringJson = JSON.stringify(respone);
+                            console.log("String json object :", this.stringJson);
+                            console.log("Type :", typeof this.stringJson);
 
 
-    // ConvertjSON to an object
+                            // ConvertjSON to an object
 
-    this.stringObject = JSON.parse(this.stringJson);
-    console.log("JSON object -", this.stringObject);
-   this.tempi= this.stringObject.locations[this.flag].currentConditions.temp
-
-   this.humidty_val=this.stringObject.locations[this.flag].currentConditions.humidity
-   this.wind_val=this.stringObject.locations[this.flag].currentConditions.wspd
-    this.moonphase=this.stringObject.locations[this.flag].currentConditions.moonphase
-   this.heatindex=this.stringObject.locations[this.flag].currentConditions.heatindex
+                            this.stringObject = JSON.parse(this.stringJson);
+                            console.log("JSON object -", this.stringObject);
+                            this.tempi = ((this.stringObject.locations[this.flag].currentConditions.temp - 32) * (5.0 / 9.0)).toFixed(1);
 
 
-   console.log("helo temp");
-   console.log(this.tempi);
-          
-          // this.weatherData=respone;        // console.log(typeof(this.weatherData))
+                            this.humidty_val = this.stringObject.locations[this.flag].currentConditions.humidity
+                            this.wind_val = this.stringObject.locations[this.flag].currentConditions.wspd
+                            this.moonphase = this.stringObject.locations[this.flag].currentConditions.moonphase
+                            this.heatindex = this.stringObject.locations[this.flag].currentConditions.heatindex
+                            // this.alerts = this.stringObject.locations[this.flag].alerts
+                            this.alerts = "jid"
+
+
+
+                            console.log("helo temp");
+                            console.log(this.tempi);
+
+                            // this.weatherData=respone;        // console.log(typeof(this.weatherData))
+                            
 
         }
-       })
+       
+      })
   }
+  
+   
+ 
 
 }
